387575eaf6481628b79da0de1c42347a3e2cdb5b
csy <786538586@qq.com>
2017-06-30 11:41:51 +0800

    自定义可移动的折线图

--- app/src/main/java/csy/com/mycharview/waveformline/WaveActivity.java
+++ app/src/main/java/csy/com/mycharview/waveformline/WaveActivity.java
@@ -2,6 +2,7 @@ package csy.com.mycharview.waveformline;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Random;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import csy.com.mycharview.R;
+import csy.com.mycharview.utils.Dbug;
 import csy.com.mycharview.utils.ScreenUtils;
 import csy.com.mycharview.waveformline.bean.WavePoint;
 
@@ -30,14 +32,49 @@ public class WaveActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wave);
         ButterKnife.bind(this);
         pointsList = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {//模拟100个数据
+        for (int i = 0; i < 400; i+=1) {//模拟100个数据
             Random random = new Random();
             WavePoint wavePoint = new WavePoint();
             wavePoint.setX(i);
             wavePoint.setY(random.nextInt(20));
             pointsList.add(wavePoint);
         }
+        waveLine.setPerPointXSpase(1);
+        //waveLine.setPerPointYSpase(2); Y的间隔不是连续的
         waveLine.setBaseLine(ScreenUtils.getScreenHeightWithoutStatiusBar(WaveActivity.this));
         waveLine.setPoints(pointsList);
     }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Dbug.d(getClass().getSimpleName(),"==ACTION_DOWN=="+event.getX() +"==="+event.getY());
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                Dbug.d(getClass().getSimpleName(),"==ACTION_MOVE=="+event.getX() +"==="+event.getY());
+                break;
+
+            case MotionEvent.ACTION_UP:
+                Dbug.d(getClass().getSimpleName(),"==ACTION_UP=="+event.getX() +"==="+event.getY());
+                break;
+
+            default:
+                break;
+        }
+//        return super.onTouchEvent(event);
+        return false;//super和false代表该事件没被消费 会继续往下传递  此处  ACTION_MOVE和 ACTION_UP不会被执行
+
+//        return true;//返回true 或者 false 事件就被消费了（终止传递） 此处  ACTION_MOVE和 ACTION_UP会执行
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Dbug.d(getClass().getSimpleName(),"==dispatchTouchEvent==");
+        return super.dispatchTouchEvent(ev);
+    }
+    //viewgroup才有
+    //onInterceptTouchEvent
 }
--- app/src/main/java/csy/com/mycharview/waveformline/WaveLineView.java
+++ app/src/main/java/csy/com/mycharview/waveformline/WaveLineView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 import java.util.List;
 
@@ -21,11 +22,28 @@ public class WaveLineView extends BaseWaveLineView {
 
     private int smallSpaceX = 20;//水平方向一小格代表多少像素
     private int smallSpaceY = 20;//垂直方向一小格代表多少像素
+
+    public int getPerPointXSpase() {
+        return perPointXSpase;
+    }
+
+    public void setPerPointXSpase(int perPointXSpase) {
+        this.perPointXSpase = perPointXSpase;
+    }
+
+    private int perPointXSpase =0;//每个点水平方向代表的小格子 由数据点的间隔决定
+    private int perPointYSpase =0;
     private String gridPaintColor;//网格颜色
     private int gridPaintStrokeWidth;//网格画笔粗细
     private String dataPaintColor;//数据颜色
     private int dataPaintStrokeWidth;//数据粗细
-
+    private String xPaintColor;//X轴数据颜色
+    private int xPaintStrokeWidth;//X轴数据粗细
+    private float actionDownStartX = 0;//手指按下时的x坐标
+    private float actionMoveDis = 0;//手指水平方向移动的距离
+    private float moveMaxX = 0;//第一个点可移动的最大距离
+    private float moveMinX = 0;//第一个点可移动的最小距离
+    private float xRoundPoint = 0;//圆点坐标 移动过程中可能会变
     public int getBaseLine() {
         return baseLine;
     }
@@ -34,7 +52,13 @@ public class WaveLineView extends BaseWaveLineView {
         this.baseLine = baseLine;
         Dbug.d(getClass().getSimpleName(),"==="+baseLine);
     }
+    public int getPerPointYSpase() {
+        return perPointYSpase;
+    }
 
+    public void setPerPointYSpase(int perPointYSpase) {
+        this.perPointYSpase = perPointYSpase;
+    }
     private int baseLine = 0;//基线
 
     public void setPoints(List<WavePoint> pointsList) {
@@ -69,6 +93,11 @@ public class WaveLineView extends BaseWaveLineView {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         //高度一屏显示完,宽度自适应
+        baseLine = getHeight();
+        Dbug.d("","==baseLine=="+baseLine);
+        moveMinX = getWidth() - pointsList.size() * smallSpaceX;
+        Dbug.d("","==baseLine=="+moveMinX);
+
     }
 
     @Override
@@ -76,6 +105,7 @@ public class WaveLineView extends BaseWaveLineView {
         super.onDraw(canvas);
         drawGrid(canvas);
         drawData(canvas);
+        drawText(canvas);
     }
 
     //画数据 数据转换为像素 *smallSpace
@@ -90,16 +120,21 @@ public class WaveLineView extends BaseWaveLineView {
         //getmPaint().setStyle(Paint.Style.FILL_AND_STROKE);//实心填充
 
         Path dataPath = new Path();
-        dataPath.moveTo(pointsList.get(0).getX(), pointsList.get(0).getY() * smallSpaceY);//移动画笔位置到第一个点位置,默认是从0,0开始的
-        for (int i = 0; i < pointsList.size() && pointsList.get(i).getX() * smallSpaceX < getWidth(); i++) {
+        for (int i = 0; i < pointsList.size(); i++) {
             //1小格代表数字1 x方向每小格多少像素  y轴方向每小格多少像素
             float y =  pointsList.get(i).getY() * smallSpaceY;
-            dataPath.lineTo(pointsList.get(i).getX() * smallSpaceX,y);
+            y = Math.abs(y-baseLine);
+            float x = pointsList.get(i).getX() * smallSpaceX;
+            //x = x + xRoundPoint*smallSpaceX;
+            x = x + xRoundPoint;//偏移量
+            if (i==0){
+                dataPath.moveTo(x,y);//这样第一个点才会跟着动
+            }else{
+                dataPath.lineTo(x,y);
+            }
         }
         //dataPath.close();//封闭  和不封闭 的图像相差大 一般用于比如三角形
         canvas.drawPath(dataPath, getmPaint());//绘制路径
-
-
     }
 
     /**
@@ -117,5 +152,96 @@ public class WaveLineView extends BaseWaveLineView {
         for (int j = 0; j < getWidth(); j = j + smallSpaceY) {//画垂直网格
             canvas.drawLine(j, 0, j, getHeight(), getmPaint());
         }
+    }
+
+    private void drawText(Canvas canvas){
+        getmPaint().setColor(Color.parseColor(dataPaintColor));
+        getmPaint().setStrokeWidth(dataPaintStrokeWidth);
+        getmPaint().setAntiAlias(true);// 消除锐化  不然线条毛糙
+        getmPaint().setStyle(Paint.Style.STROKE);//一定要设置样式为STROKE 空心 不然默认为实心,画出来的一团黑
+        for (int i = 0; i < pointsList.size(); i++) {
+            //1小格代表数字1 x方向每小格多少像素  y轴方向每小格多少像素
+            float y =  pointsList.get(i).getY() * smallSpaceY;
+            y = Math.abs(y-baseLine);
+            float x = pointsList.get(i).getX() * smallSpaceX;
+            //x = x + xRoundPoint*smallSpaceX;
+            x = x + xRoundPoint;//偏移量
+            canvas.drawText(""+pointsList.get(i).getX(),x,getHeight()-smallSpaceY,getmPaint());//画上文字
+
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                //Dbug.d(getClass().getSimpleName(),"==ACTION_DOWN=="+event.getX() +"==="+event.getY());
+                actionDownStartX = event.getX();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                //如果所有点的宽度不足一屏 则不可滑动
+                if (smallSpaceX*pointsList.size()<=getWidth()){
+                    return true;
+                }
+                //需要加判断 移动到左边第一个点的时候不在移动  移动到右边第一个点的时候不让其再移动
+                //Dbug.d(getClass().getSimpleName(),"==ACTION_MOVE=="+event.getX() +"==="+event.getY());
+                actionMoveDis = (int)(event.getX() - actionDownStartX);
+                actionDownStartX = event.getX();//有时候移动不放手
+//                moveMinX = smallSpaceX;
+//                if (actionMoveDis>=0 && actionMoveDis<moveMinX){//每个点移动的最小距离是 屏幕宽度/点数
+//                    actionMoveDis = moveMinX;
+//                    Dbug.d("ACTION_MOVE","===actionMoveDis=1="+actionMoveDis);
+//                }else if (actionMoveDis<0 && actionMoveDis >-moveMinX){
+//                    actionMoveDis = - moveMinX;
+//                    Dbug.d("ACTION_MOVE","===actionMoveDis=2="+actionMoveDis);
+//                }else{
+//                    Dbug.d("ACTION_MOVE","===actionMoveDis=3="+actionMoveDis);
+//                }
+                actionMoveDis = (actionMoveDis%smallSpaceX)*smallSpaceX;//保证移动的为几小格
+                if ((int)actionMoveDis==0){
+                    actionMoveDis = smallSpaceX;
+                }
+                xRoundPoint +=actionMoveDis;
+                Dbug.d("==","====bord=1="+-(pointsList.size()*smallSpaceX*perPointXSpase - getWidth()));
+                Dbug.d("==","====bord=2="+xRoundPoint+"====actionMoveDis=="+actionMoveDis);
+                if (xRoundPoint>0 ){
+                    xRoundPoint -= actionMoveDis;
+                }else if (xRoundPoint< -(pointsList.size()*smallSpaceX*perPointXSpase - getWidth())){
+                    xRoundPoint -= actionMoveDis;
+                }else{
+                    invalidate();//重绘
+                }
+//                if (xRoundPoint>0 || xRoundPoint < - pointsList.size()*smallSpaceX){
+//                    Dbug.d(getClass().getSimpleName(),"==ACTION_MOVE==不能再移动啦==xRoundPoint=="+xRoundPoint);
+//                    xRoundPoint -= actionMoveDis;
+//                }else{
+//                    invalidate();//重绘
+//                }
+//                invalidate();//重绘
+
+                //边界判断
+                Dbug.d(getClass().getSimpleName(),"==ACTION_MOVE==dis=="+actionMoveDis);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                //Dbug.d(getClass().getSimpleName(),"==ACTION_UP=="+event.getX() +"==="+event.getY());
+                break;
+
+            default:
+                break;
+        }
+//        return super.onTouchEvent(event);
+//        return false;//super和false代表该事件没被消费 会继续往下传递  此处  ACTION_MOVE和 ACTION_UP不会被执行
+
+        return true;//返回true 或者 false 事件就被消费了（终止传递） 此处  ACTION_MOVE和 ACTION_UP会执行   activity里面的onTouchEvent也将不会执行
+    }
+
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        Dbug.d(getClass().getSimpleName(),"==dispatchTouchEvent==");
+        return super.dispatchTouchEvent(event);
+//        return true;//此处返回true不会走onTouchEvent了
     }
 }
