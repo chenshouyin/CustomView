625d44fe91677675e74589afaceec651e75a17a7
csy <786538586@qq.com>
2017-09-01 09:09:54 +0800

    MyCharView

---
+++ app/src/main/java/csy/com/mycharview/MyWaveViewActivity1.java
@@ -0,0 +1,29 @@
+package csy.com.mycharview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import csy.com.mycharview.base.BaseActivity;
+
+/**
+ * Created by user on 2017-8-8.
+ */
+
+public class MyWaveViewActivity1 extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wave_activity1);
+    }
+
+    @Override
+    public void addAction() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
